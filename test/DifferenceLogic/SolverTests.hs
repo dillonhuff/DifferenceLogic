22be2412516e24e37ea47f547d48ebc3ff4a88a3
Dillon Huff <dillonhuff@gmail.com>
2014-10-24 13:42:25 -0500

    added more test cases

--- test/DifferenceLogic/SolverTests.hs
+++ test/DifferenceLogic/SolverTests.hs
@@ -9,4 +9,8 @@ allSolverTests = do
 tSatCases =
   [(zF [zLit "a" "b" eq 10], True),
    (zF [zLit "a" "b" eq 10, zLit "a" "b" lt 10], False),
-   (zF [zLit "a" "b" leq 20, zLit "a" "b" geq 30], False)]
+   (zF [zLit "a" "b" leq 20, zLit "a" "b" geq 30], False),
+   (zF [zLit "a" "b" lt (-23), zLit "a" "b" gt (-22)], False),
+   (zF [zLit "a" "b" lt 54, zLit "a" "b" gt 52], True),
+   (zF [zLit "c" "d" eq 0, zLit "a" "c" lt 10, zLit "a" "d" gt 4], True),
+   (zF [zLit "c" "d" eq 0, zLit "a" "c" lt 10, zLit "a" "d" geq 11], False)]
